92ad8e14d47d3504c5a07493d10ad1189445552f
OdaSeiji <hbier2009@gmail.com>
2023-03-28 21:44:09 +0700

    Making Production page

--- php/ProductionNumber/SelCategory2V2.php
+++ php/ProductionNumber/SelCategory2V2.php
@@ -17,24 +17,24 @@
 
     $prepare = $dbh->prepare("
 
-SELECT 
-  m_production_numbers_category2.id,
-  SUBSTRING(m_production_numbers_category2.name_jp, 1, 9) AS name_jp,
-  IFNULL(t1.count, 0) as count
-FROM 
- m_production_numbers_category2
-LEFT JOIN 
-(
-SELECT 
- m_production_numbers.production_category2_id, COUNT(*) AS count
-FROM 
- m_production_numbers
-WHERE 
- m_production_numbers.production_category2_id IS NOT NULL
-GROUP BY 
- m_production_numbers.production_category2_id
-) AS t1 ON m_production_numbers_category2.id = t1.production_category2_id
-WHERE m_production_numbers_category2.category1_id = :targetId
+      SELECT 
+        m_production_numbers_category2.id,
+        SUBSTRING(m_production_numbers_category2.name_jp, 1, 9) AS name_jp,
+        IFNULL(t1.count, 0) as count
+      FROM 
+      m_production_numbers_category2
+      LEFT JOIN 
+        (
+          SELECT 
+            m_production_numbers.production_category2_id, COUNT(*) AS count
+          FROM 
+            m_production_numbers
+          WHERE 
+            m_production_numbers.production_category2_id IS NOT NULL
+          GROUP BY 
+            m_production_numbers.production_category2_id
+        ) AS t1 ON m_production_numbers_category2.id = t1.production_category2_id
+      WHERE m_production_numbers_category2.category1_id = :targetId
 
    ");
 
--- scr/ProdcutionNumberV3.js
+++ scr/ProdcutionNumberV3.js
@@ -25,7 +25,6 @@ $(function () {
   // make summary table
   readSummaryTable();
   readCategory1Table();
-  console.log("hello");
 });
 
 function readSummaryTable() {
@@ -70,7 +69,7 @@ function readCategory1Table() {
   });
 }
 
-$(document).on("keyup", "#production_name", function () {
+$(document).on("keyup", "#production_number", function () {
   $(this).val($(this).val().toUpperCase()); // 小文字を大文字に
   if ($(this).val().length > 3) {
     $(this).removeClass("input-required");
@@ -161,19 +160,25 @@ $(document).on("change", ".top__wrapper", function () {
   }
 });
 
+$(document).on("click", ".top__wrapper", function () {
+  // console.log(checkInput());
+  if (checkInput()) {
+    $("#save__button").prop("disabled", false);
+  } else {
+    $("#save__button").prop("disabled", true);
+  }
+});
+
 function checkInput() {
   let flag = true;
-  // console.log("hello");
-  // console.log($(".save-data"));
-
   $(".save-data").each(function (index, element) {
-    // console.log(element.attr(");
-    // console.log($(element).attr("class"));
-    // console.log($(element).hasClass("input-required"));
     if ($(element).hasClass("input-required")) {
       flag = false;
     }
   });
+  if ($(".top__wrapper table tbody tr.selected-record").length != 2) {
+    flag = false;
+  }
   return flag;
 }
 
@@ -202,14 +207,15 @@ $(document).on("click", "#category1__table tbody tr", function () {
   sendData = {
     targetId: $("#category1__tr").find("td").eq(0).html(),
   };
+  console.log(sendData);
   myAjax.myAjax(fileName, sendData);
 
+  $("#category2__table tbody").empty();
   ajaxReturnData.forEach(function (trVal) {
     var newTr = $("<tr>");
     Object.keys(trVal).forEach(function (tdVal) {
       $("<td>").html(trVal[tdVal]).appendTo(newTr);
     });
-    $("#category2__table tbody").empty();
     $(newTr).appendTo("#category2__table tbody");
   });
 });
@@ -219,5 +225,58 @@ $(document).on("click", "#category2__table tbody tr", function () {
     "selected-record"
   );
   $("#category2__tr").removeAttr("id");
-  $(this).addClass("selected-record").attr("id", "categoryu__tr");
+  $(this).addClass("selected-record").attr("id", "category2__tr");
 });
+
+$(document).on("click", "#save__button", function () {
+  let fileName;
+  let sendData = new Object();
+
+  sendData = getInputData();
+  fileName = "./php/ProductionNumber/InsInputData2.php";
+  myAjax.myAjax(fileName, sendData);
+
+  $("#summary__table tbody").empty();
+  readSummaryTable();
+});
+
+$(document).on("click", "#test__button", function () {
+  console.log("helo");
+  let tableObj = new Object();
+  let inputProductionNumber = "C2Q63A-AD141A20K";
+  let tableProdcutionNumber;
+  tableObj = $("#summary__table tbody tr");
+  tableObj.each(function (index, element) {
+    tableProdcutionNumber = $(element).find("td").eq(3).html();
+    console.log($(element).find("td").eq(3).html());
+    if ($(element).find("td").eq(3).html() == inputProductionNumber) {
+      $(element).addClass("selected-record");
+    }
+  });
+  tableObj.scrollTop(300);
+  console.log(tableObj.scrollTop());
+});
+
+function getInputData() {
+  let inputData = new Object();
+  let category2 = $("#category2__tr").find("td").eq(0).html();
+  let dt = new Date();
+  // .save-dataを持っている要素から値を取り出す
+  $("input.save-data").each(function (index, element) {
+    inputData[$(this).attr("id")] = $(this).val();
+  });
+  $("select.save-data").each(function (index, element) {
+    inputData[$(this).attr("id")] = $(this).val();
+  });
+  // 日付はYY-mm-dd形式なのでYYYY-mm-dd形式に変更
+  // inputData["date__input"] = "20" + inputData["date__input"];
+  // targetId を別途保存
+  inputData["targetId"] = $("#summary__tr").find("td").eq(0).html();
+  // production category2 id の取得
+  inputData["production_category2_id"] = category2;
+  // 今日の日付の取得
+  inputData["updated_at"] =
+    dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
+
+  return inputData;
+}
