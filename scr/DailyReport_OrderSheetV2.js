128caebbacaefa34c819305170254a69d1d81dfa
Oda Seiji <hbier2009@gmail.com>
2024-06-18 22:43:53 +0900

    fix

--- scr/DailyReport_OrderSheetV2.js
+++ scr/DailyReport_OrderSheetV2.js
@@ -47,6 +47,7 @@ function ajaxSelSummary() {
 }
 
 function makeSummaryTable(data) {
+  let intQty;
   $("#summary__table tbody").empty();
   data.forEach(function (trVal) {
     var newTr = $("<tr>");
@@ -54,10 +55,32 @@ function makeSummaryTable(data) {
       /(\d)(?=(\d\d\d)+(?!\d))/g,
       "$1,"
     );
-    Object.keys(data[0]).forEach(function (tdVal) {
-      $("<td>").html(trVal[tdVal]).appendTo(newTr);
+
+    Object.keys(data[0]).forEach(function (tdVal, index) {
+      if (index == 5) {
+        intQty = trVal[tdVal];
+        // console.log(intQty);
+      }
+      if (index != 7) {
+        $("<td>").html(trVal[tdVal]).appendTo(newTr);
+      } else {
+        // console.log(trVal[tdVal] + ":" + intQty);
+        if (trVal[tdVal] <= intQty) {
+          $("<td>").html(trVal[tdVal]).addClass("packedOk").appendTo(newTr);
+          // console.log("OK");
+        } else {
+          $("<td>").html(trVal[tdVal]).appendTo(newTr);
+        }
+      }
     });
     $("#summary__table tbody").append($(newTr));
   });
-  make_action();
+  // make_action();
 }
+
+$(document).on("click", "#summary__table tbody tr", function () {
+  const targetTr = $(this).find("td");
+  const targetId = targetTr.eq(0).text();
+  $("#summary__table tr.selected-record").removeClass("selected-record");
+  $(this).addClass("selected-record");
+});
