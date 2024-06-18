aeec1ef4ac54cdf9d7cda42cada483940e600537
Oda Seiji <hbier2009@gmail.com>
2024-06-18 13:21:25 +0900

    php fixed

--- php/OrderSheet/SelSummarySubV2.php
+++ php/OrderSheet/SelSummarySubV2.php
@@ -17,54 +17,63 @@
 
     $prepare = $dbh->prepare("
 
-    SELECT 
-    m_ordersheet.id,
-    m_ordersheet.ordersheet_number,
-    m_ordersheet.delivery_date_at,
-    m_ordersheet.issue_date_at,
-    m_production_numbers.production_number,
-    m_ordersheet.production_quantity,
-    IFNULL(FORMAT((SUM(t10.work_quantity) - SUM(t10.total_ng)),
-                0),
-            0) AS total_ok,
-    FORMAT(t20.packed_work_quantitiy, 0) AS packed,
-    m_ordersheet.note
+
+with subT1 as (
+SELECT 
+    t_press.ordersheet_id as ordersheet_id,
+    SUM(IFNULL(t1.sum_cut_qty, 0) - IFNULL(t2.sum_ng_qty, 0)) AS sum_ok_qty,
+    SUM(IFNULL(t1.sum_cut_qty, 0)) AS sum_cut_qty
 FROM
-    m_ordersheet
-        LEFT JOIN
-    t_press ON t_press.ordersheet_id = m_ordersheet.id
-        LEFT JOIN
-    m_production_numbers ON m_ordersheet.production_numbers_id = m_production_numbers.id
+    t_press
         LEFT JOIN
     (SELECT 
         t_using_aging_rack.t_press_id,
-            SUM(IFNULL(t_using_aging_rack.work_quantity, 0)) AS work_quantity,
-            SUM(IFNULL((SELECT 
-                    SUM(t_press_quality.ng_quantities)
-                FROM
-                    t_press_quality
-                WHERE
-                    t_press_quality.using_aging_rack_id = t_using_aging_rack.id
-                GROUP BY t_press_quality.using_aging_rack_id), 0)) AS total_ng
+            SUM(IFNULL(t_using_aging_rack.work_quantity, 0)) AS sum_cut_qty
     FROM
         t_using_aging_rack
-    GROUP BY t_using_aging_rack.t_press_id) t10 ON t10.t_press_id = t_press.id
+    WHERE
+        t_using_aging_rack.t_press_id IS NOT NULL
+    GROUP BY t_using_aging_rack.t_press_id) AS t1 ON t1.t_press_id = t_press.id
         LEFT JOIN
     (SELECT 
-        m_ordersheet.id AS m_ordersheet_id,
-            m_ordersheet.ordersheet_number,
-            m_ordersheet.delivery_date_at,
-            m_ordersheet.production_quantity,
-            IFNULL(SUM(t_packing_box.work_quantity), 0) AS packed_work_quantitiy,
-            m_production_numbers.production_number
+        t_using_aging_rack.t_press_id,
+            SUM(IFNULL(t_press_quality.ng_quantities, 0)) AS sum_ng_qty
     FROM
-        m_ordersheet
-    LEFT JOIN t_packing_box_number ON t_packing_box_number.m_ordersheet_id = m_ordersheet.id
-    LEFT JOIN t_packing_box ON t_packing_box.box_number_id = t_packing_box_number.id
-    LEFT JOIN m_production_numbers ON m_ordersheet.production_numbers_id = m_production_numbers.id
-    GROUP BY m_ordersheet.id) t20 ON t20.m_ordersheet_id = m_ordersheet.id
-GROUP BY m_ordersheet.id
-ORDER BY issue_date_at DESC , delivery_date_at DESC , ordersheet_number DESC;
+        t_using_aging_rack
+    LEFT JOIN t_press_quality ON t_press_quality.using_aging_rack_id = t_using_aging_rack.id
+    WHERE
+        t_using_aging_rack.t_press_id IS NOT NULL
+    GROUP BY t_using_aging_rack.t_press_id) AS t2 ON t2.t_press_id = t_press.id
+where t_press.ordersheet_id is not null
+group by t_press.ordersheet_id 
+),
+subT2 as (
+SELECT 
+    t_packing.m_ordersheet_id as ordersheet_id,
+    sum(t_packing_box.work_quantity) as sum_packed_qty
+FROM
+    t_packing
+        LEFT JOIN
+    t_packing_box ON t_packing_box.packing_id = t_packing.id
+group by t_packing.m_ordersheet_id
+)
+select 
+	m_ordersheet.id,
+    m_ordersheet.ordersheet_number,
+    date_format(m_ordersheet.delivery_date_at, '%y-%m-%d') as delivery_date_at,
+    date_format(m_ordersheet.issue_date_at, '%y-%m-%d') as issue_date_at,
+    m_production_numbers.production_number,
+    m_ordersheet.production_quantity,
+    ifnull(subT1.sum_ok_qty, 0) as sum_ok_qty,
+    ifnull(subT2.sum_packed_qty, 0) as sum_packed_qty
+from 
+	m_ordersheet
+left join subT1 on subT1.ordersheet_id = m_ordersheet.id
+left join subT2 on subT2.ordersheet_id = m_ordersheet.id
+left join m_production_numbers on m_ordersheet.production_numbers_id = m_production_numbers.id
+WHERE YEAR(m_ordersheet.delivery_date_at) = YEAR(CURDATE())
+order by m_ordersheet.id desc
+        ;
     ");
     $prepare->execute();
     $result = $prepare->fetchALL(PDO::FETCH_ASSOC);
