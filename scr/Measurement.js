10f5219b55fa1410d3482650afbb8c1595376858
MVC0712 <64857148+MVC0712@users.noreply.github.com>
2024-11-14 11:40:42 +0900

    Add name to measument

--- scr/Measurement.js
+++ scr/Measurement.js
@@ -4,6 +4,8 @@ let meaStaff =[{id: 30, staff_name: "Trần Thị Thảo"},
                 {id: 72, staff_name: "Hồ Thị Thủy"},
                 {id: 41, staff_name: "Nguyễn Thị Vui"},
                 {id: 60, staff_name: "Đinh Kiều Diễm"},
+                {id: 82, staff_name: "Trần Thị Mỹ Hà"},
+                {id: 83, staff_name: "Nguyễn Thị Diễm Trinh"},
             ];
 
 let imageList = [];
