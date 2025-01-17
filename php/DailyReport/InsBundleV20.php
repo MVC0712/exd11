f6bf5c72201b1dd39a00d401818b2b75def8d7ae
MVC0712 <64857148+MVC0712@users.noreply.github.com>
2025-01-17 08:17:10 +0700

    Press machine No.3

---
+++ php/DailyReport/InsBundleV20.php
@@ -0,0 +1,36 @@
+<?php
+  $userid = "webuser";
+  $passwd = "";
+
+  $data = file_get_contents('php://input');
+  $data_json = json_decode($data); 
+
+  $selected_id = array_pop($data_json);
+
+  try{
+    $dbh = new PDO(
+      'mysql:host=localhost; dbname=extrusion; charset=utf8',
+      $userid,
+      $passwd,
+      array(
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_EMULATE_PREPARES => false
+      )
+    );
+
+    if(count($data_json) > 0){
+      foreach($data_json as $val){
+        $sql_paramater[] = "('{$selected_id}', '{$val[1]}', '{$val[2]}', '{$val[3]}', '{$val[4]}', '{$val[5]}')";
+      }
+      $sql = "INSERT INTO t_bundle (press_id, bundle, quantity, lot, len, mfg) VALUES ".join(",", $sql_paramater);
+      $prepare = $dbh->prepare($sql);
+      $prepare->execute();
+    }
+
+    echo json_encode("INSERTED");
+  } catch (PDOException $e){
+    $error = $e->getMessage();
+    print_r($error);
+  }
+  $dbh = null;
+?>
---
+++ php/DailyReport/UpdateBundleV20.php
@@ -0,0 +1,48 @@
+<?php
+  /* 21/09/11 */
+  $userid = "webuser";
+  $passwd = "";
+
+  $bundle = "";
+  $quantity = "";
+  $lot = "";
+  $id = "";
+
+  $bundle = $_POST['bundle'];
+  $quantity = $_POST['quantity'];
+  $lot = $_POST['lot'];
+  $len = $_POST['len'];
+  $mfg = $_POST['mfg'];
+  $id = $_POST['id'];
+
+  try{
+    $dbh = new PDO(
+      'mysql:host=localhost; dbname=extrusion; charset=utf8',
+      $userid,
+      $passwd,
+      array(
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_EMULATE_PREPARES => false
+      )
+    );
+
+    $sql = "UPDATE t_bundle
+              SET 
+                bundle = '$bundle',
+                quantity = '$quantity',
+                lot = '$lot',
+                len = '$len',
+                mfg = '$mfg'
+            WHERE t_bundle.id = $id
+    ";
+    // print_r($sql);
+    $prepare = $dbh->prepare($sql);
+    $prepare->execute();
+
+    echo json_encode("INSERTED");
+  } catch (PDOException $e){
+    $error = $e->getMessage();
+    print_r($error);
+  }
+  $dbh = null;
+?>
