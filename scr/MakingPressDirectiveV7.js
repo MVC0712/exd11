e66d0948d380b9b322fa7ff66deae4dffc19dcbf
Oda Seiji <hbier2009@gmail.com>
2025-01-23 06:48:20 +0900

    adj V7

--- scr/MakingPressDirectiveV7.js
+++ scr/MakingPressDirectiveV7.js
@@ -625,8 +625,8 @@ $(document).on("click", "#make-pdf__button", function () {
 
 function makeNewPage() {
   // 読み込むHTMLファイルのパス
-  // const htmlFilePath = "../exd11/PressDSPDF.html";
-  const htmlFilePath = "../ex0.11/PressDSPDF.html"; // in SMC
+  const htmlFilePath = "../exd11/PressDSPDF.html";
+  // const htmlFilePath = "../ex0.11/PressDSPDF.html"; // in SMC
 
   const replacements = getPrintData();
   // return;
@@ -781,7 +781,7 @@ function getPrintData() {
   printDataValues["${second_qty}"] = $("#second-profile__input").val();
 
   printDataValues["${makeTable()}"] = makeProfileTable();
-
+  // console.log(printDataValues);
   // console.log(printDataValues);
   return printDataValues;
 }
